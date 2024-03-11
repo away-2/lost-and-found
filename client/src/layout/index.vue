e4860111cbf53aea4f58259584dc850aa0e49d22
marineMoon <2934386052@qq.com>
2024-03-11 10:27:50 +0800

    顶部导航伸缩样式

--- client/src/layout/index.vue
+++ client/src/layout/index.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="layout_container">
-        <div class="layout_header" v-if="!showScrollTop">
+        <div class="layout_header" :class="{ 'hidden': showScrollTop }">
             <Header />
         </div>
         <div class="layout_main">
@@ -34,8 +34,13 @@ onMounted(() => {
     .layout_header{
         position: fixed;
         width: 100%;
+        transform: translateY(0);
         z-index: 250;
+        transition: all 0.5s;
+        &.hidden {
+            transform: translateY(-70px);
         }
+    }
     .layout_main{
         background: #f1f1f1;
         padding-top: @base-tabbar-height;
